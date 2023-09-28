cbbe10ea042bdd289e72ef0255d345c6922460d8
tamas <tamasnagy1976@gmail.com>
2023-09-28 14:57:34 +0200

    refactor test_server

--- test_server.py
+++ test_server.py
@@ -1,11 +1,34 @@
 import requests
+from requests.exceptions import RequestException
 
-print(
-    requests.post(
-        # "http://localhost:10000",
-        "http://127.0.0.1:10000",
-        json={
-            "query": "what is nicotinamide riboside?",
-        }
-    ).json()
-)
+# BASE_URL = "http://localhost:10000"
+# BASE_URL = "http://127.0.0.1:10000"
+BASE_URL = 'https://research-agent2.onrender.com'
+
+
+if False:
+    response = requests.post(
+            BASE_URL,
+            json={
+                "query": "what is nicotinamide riboside?",
+            }
+    )
+    response = response.json()
+    print(response)
+
+
+def test_web_service(query):
+    payload = {"query": query}
+    try:
+        response = requests.post(BASE_URL, json=payload)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+        return response.json()
+    except RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    query = "what is nicotinamide riboside?"
+    # response = test_web_service(query)
+    # if response is not None:
+    #     print(response)
